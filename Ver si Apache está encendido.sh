87addf0aa939b5d926187d57bb15f34986e79029
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-03-27 16:57:10 +0100

    Update Ver si Apache está encendido.sh

--- Ver si Apache está encendido.sh
+++ Ver si Apache está encendido.sh
@@ -1,7 +1,1 @@
-# !/bin/bash
-if [ `pgrep apache2 -c` -le "0" ]; then
-  /etc/init.d/apache2 stop
-  pkill -u www-data  
-  /etc/init.d/apache2 start
-  echo "Arrancando Apache"
-fi
+* https://www.jesusninoc.com/03/21/ver-si-apache-esta-encendido/

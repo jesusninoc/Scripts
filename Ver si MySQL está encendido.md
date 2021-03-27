50b42c9ad41afdb861bd97efacbab32828ddfb5f
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-03-27 16:58:35 +0100

    Update and rename Ver si MySQL está encendido.sh to Ver si MySQL está encendido.md

---
+++ Ver si MySQL está encendido.md
@@ -0,0 +1,1 @@
+* https://www.jesusninoc.com/03/20/ver-si-mysql-esta-encendido/
--- Ver si MySQL está encendido.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-if [ `pgrep mysql -c` -le "0" ]; then
-  /etc/init.d/mysql stop
-  /etc/init.d/mysql start
-  echo "Arrancando MySQL"
-fi
